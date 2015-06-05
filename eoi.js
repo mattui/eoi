6d6d679fabceef0d06569ec92eba97aec48374a0
Matt Klepac <mklepac@zynga.com>
2015-06-05 09:56:14 -0500

    Switched to MutationObserver and added support for changing the tip

--- eoi.js
+++ eoi.js
@@ -25,25 +25,44 @@
         var $TAX = 0.083;
         var $TIP = 0.1;
 
-        var getLimit = function() { return $BUDGET/(1 + $TAX)/(1 + $TIP); }
         var isCartEmpty = $('#total').length == 0;
+        var getTip = function() {
+            var tip = $('#CartGratuity');
+            if (tip.length == 0) {
+                return $TIP;
+            }
+            else
+            {
+                return tip.val() / 100;
+            }
+        };
 
-        var changing = false;
+        var changing = 0;
 
-        $('#cart').bind('DOMSubtreeModified', function(e) {
-            if (!changing) {
+        var forceChange = function(fn) {
+            changing++;
+            fn();
+            changing--;
+        };
+
+        var onCartChange = function() {
+            if (changing == 0) {
                 requestAnimationFrame(function() {
                     isCartEmpty = $('#total').length == 0;
                     console.log('CHANGE!', isCartEmpty);
                     updateBudget();
-                    changing = false;
+                    changing--;
                 });
+            changing++;
             }
-            changing = true;
-        });
+        };
+
+        var observer = new MutationObserver(onCartChange);
+        observer.observe($('#cart').get(0), { attributes: true, childList: true, characterData: true });
 
         var updateBudget = function() {
             var formatDollars = function(dollars) {
+                dollars = dollars.toFixed(2);
                 var dpos = dollars.toString().indexOf('.');
                 if (dpos == -1) {
                     return dollars + '.00';
@@ -53,22 +72,29 @@
                 }
                 return dollars;
             };
+
             var budget = (function() {
                 if (isCartEmpty) return $BUDGET;
 
                 var total = $(":contains('Grand Total')");
                 var totalText = total.children('span').text();
+
                 var amount = parseFloat(totalText.substring(totalText.indexOf("$") + 1));
                 console.log("Grand Total", amount);
+
                 var over = '$' + (amount > $BUDGET ? formatDollars(Math.abs($BUDGET - amount)) : formatDollars(0));
-                changing = true;
-                total.parent().parent().append("<p><strong>You Pay<span>" + over + "</span></strong></p>");
-                changing = false;
                 console.log("You Pay", over);
+
+                forceChange(function() {
+                    $('.youPay, .yourBudget').remove();
+                    $('#total').append('<p class="yourBudget"><strong>Your Budget<span>$' + formatDollars($BUDGET) + '</span></strong></p>');
+                    $('#total').append('<p class="youPay"><strong>You Pay<span>' + over + '</span></strong></p>');
+                });
+
                 return $BUDGET - amount;
             })();
 
-            var getCost = function(amount) { return budget - (amount * (1 + $TAX) * (1 + $TIP)); }
+            var getCost = function(amount) { return budget - (amount * (1 + $TAX) * (1 + getTip())); }
 
             $('em')
                 .each(function() {
@@ -86,12 +112,12 @@
                             .css('background-color', cost < 0 ? 'rgba(256,0,0,0.1)' : 'rgba(0,256,0,0.1)')
                             .css('border', cost < 0 ? '1px solid red' : '1px solid green');
 
-                        var details = 'extra';
+                        var details = 'budget left:';
                         if (cost < 0) {
-                            details = 'over'
+                            details = 'over budget by:'
                         }
                         t.append('<div class="costDetails">'
-                                + details
+                                + '<i style="font-style: italic !important; padding-right: 1em;">' + details + '</i>'
                                 + " $"
                                 + formattedCost
                                 + '</div>');
